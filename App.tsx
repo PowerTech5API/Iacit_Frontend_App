cfc3d6f985fb8230da3e731a55771c51c2d9eb5f
Juliana Maria <juliana.ramos01@fatec.sp.gov.br>
2023-06-04 19:09:52 -0300

    add ConfigProvider no App

--- App.tsx
+++ App.tsx
@@ -3,16 +3,18 @@ import * as React from 'react';
 import FormProvider from './src/components/CadastroRO/formProvider';
 import AppNavigator from './src/components/MenuNavigation/AppNavigator';
 import AuthProvider from './src/components/User/AuthProvider';
-
+import {ConfigProvider} from  './src/contexts/configContext';
 
 
 export default function App() {
   
   return (
     <AuthProvider>
+    <ConfigProvider>
     <FormProvider>
           <AppNavigator/>
     </FormProvider>
+    </ConfigProvider>
     </AuthProvider>
   );
 }
